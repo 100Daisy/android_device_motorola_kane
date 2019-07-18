e278202e1bdd1e638195097590f16726486a1251
100Daisy <48625180+100Daisy@users.noreply.github.com>
2019-07-18 18:14:21 +0200

    Android Verified Boot Support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,3 +34,13 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 include $(LOCAL_PATH)/kernel.mk
 include device/generic/twrpbuilder/BoardConfig64.mk
 
+# Android Verified Boot
+BOARD_AVB_ENABLE := true
+#BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS := 
+ifeq ($(BOARD_AVB_ENABLE), true)
+   BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+   BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+   BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+   BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+endif
+
